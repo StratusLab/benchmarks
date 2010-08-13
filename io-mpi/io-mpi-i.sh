b8bcf369b5f51dd3e0ac05fd83f3cb155921e4b1
airaj <airaj@lal.in2p3.fr>
2010-08-13 16:18:36 +0200

    sprint2 benchmarks

---
+++ io-mpi/io-mpi-i.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+  echo "Usage /usr/libexec/io-mpi-i.sh Inputfile_size(Mbytes)  "
+  exit
+else
+  mpirun --mca btl tcp,self -np $1  --byslot /usr/libexec/io-mpi-i $1 | sort 
+fi
